b6c73cfa9813aca8dd70fa3634767a163a61555f
Bevan Wentzel <bevan@teamgeek.io>
2024-06-26 07:46:47 +0200

    Text in front

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import Canvas from "./Canvas";
 function App() {
   return (
     <div className="relative">
-      <div className="h-dvh w-full flex justify-center items-center">
-        <h1 className="text-xl">Hello World!</h1>
-      </div>
-
       <div className="absolute inset-0">
         <Canvas />
+      </div>
+
+      <div className="h-dvh w-full flex justify-center items-center relative">
+        <h1 className="text-xl">Hello World!</h1>
       </div>
     </div>
   );
