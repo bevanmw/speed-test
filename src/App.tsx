51ee71eb6644df47107338b90256d3a94d4d7dde
Bevan Wentzel <bevan@teamgeek.io>
2024-06-26 07:49:49 +0200

    Remove canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Canvas from "./Canvas";
+// import Canvas from "./Canvas";
 
 function App() {
   return (
     <div className="relative">
-      <div className="absolute inset-0">
-        <Canvas />
-      </div>
+      <div className="absolute inset-0">{/* <Canvas /> */}</div>
 
       <div className="h-dvh w-full flex justify-center items-center relative">
         <h1 className="text-xl">Hello World!</h1>
